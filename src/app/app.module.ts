26d92599fb8915247b5ecc477bc73f3b1023e4cd
Ajay Chaurasia <ajay.chaurasia29@gmail.com>
2022-03-27 18:09:20 +0530

    Adding Husky and Landing Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LandingComponent } from './components/landing/landing.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: LandingComponent },
+    ]),
     BrowserAnimationsModule
   ],
   providers: [],

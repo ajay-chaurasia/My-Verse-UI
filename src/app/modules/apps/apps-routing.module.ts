26de14d55a1370124b5b6ba763518a949a95bedb
Ajay Chaurasia <ajay.chaurasia29@gmail.com>
2022-12-27 19:00:54 +0530

    Adding BMI Calculator app

--- src/app/modules/apps/apps-routing.module.ts
+++ src/app/modules/apps/apps-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppsComponent } from './apps.component';
+import { BmiCalculatorComponent } from './bmi-calculator/bmi-calculator.component';
 import { BookLibraryComponent } from './book-library/book-library.component';
 
 const routes: Routes = [
   { path: '', component: AppsComponent },
+  { path: 'bmi-calculator', component: BmiCalculatorComponent },
   { path: 'book-library', component: BookLibraryComponent }
 ];
 
